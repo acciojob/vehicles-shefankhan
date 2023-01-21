287e2e9fc3fa22f35873d1bfcc20664f3213cc6c
shefankhan <shefanofficial@gmail.com>
2023-01-21 22:00:14 +0530

    first

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,36 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
+
         //Hint: Car extends Vehicle
+
+         this.wheels=wheels;
+         this.doors=doors;
+         this.gears=gears;
+         this.isManual=isManual;
+         this.seats=seats;
+         this.type=type;
+         this.currentGear=1;
     }
 
     public void changeGear(int newGear){
-
+              this.currentGear=newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+               move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+       super(name,4,2,6,isManual,"petrol",6);
     }
 
     public void accelerate(int rate){
@@ -17,14 +18,26 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        newSpeed=getCurrentSpeed()+rate;
+
+
+
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setGears(0);
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            if(newSpeed>=1 && newSpeed<=50) changeGear(1);
+            if(newSpeed>=51 && newSpeed<=100)changeGear(2);
+            if(newSpeed>=101 && newSpeed<=150)changeGear(3);
+            if(newSpeed>=151 && newSpeed<=200)changeGear(4);
+            if(newSpeed>=201 && newSpeed<=250)changeGear(5);
+            if(newSpeed>=251)changeGear(6);
         }
     }
 }
