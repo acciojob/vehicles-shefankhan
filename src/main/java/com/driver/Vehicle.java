0628fdad39435c0bbaa1a6e234147bdcd7983709
shefankhan <shefanofficial@gmail.com>
2023-01-21 23:48:04 +0530

    final

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,6 +6,38 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
+    public Vehicle(String name) {
+        this.name=name;
+        this.currentDirection=0;
+        this.currentSpeed=0;
+    }
+
+    public void steer(int direction){
+        //direction is in degrees, add it to the current direction
+        currentDirection+=direction;
+        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void move(int speed, int direction){
+        //set the values of currentSpeed and currentDirection
+        currentSpeed=speed;
+        currentDirection=direction;
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void stop(){
+        currentSpeed=0;
+        System.out.println("stop method called - The vehicle is stopped");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
@@ -21,29 +53,4 @@ public class Vehicle {
     public void setCurrentDirection(int currentDirection) {
         this.currentDirection = currentDirection;
     }
-
-    public Vehicle(String name) {
-          this.name=name;
-          this.currentSpeed=0;
-          this.currentDirection=0;
-    }
-
-    public void steer(int direction){
-        //direction is in degrees, add it to the current direction
-        this.currentDirection=direction;
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void move(int speed, int direction){
-        //set the values of currentSpeed and currentDirection
-        this.currentSpeed=speed;
-        this.currentDirection=direction;
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void stop(){
-        this.currentSpeed=0;
-        System.out.println("stop method called - The vehicle is stopped");
-    }
-
 }
